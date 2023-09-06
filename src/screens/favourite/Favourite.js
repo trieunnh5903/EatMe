fb14f89876e54af454d616f97c87d20b447bce49
trieunnh5903 <111626907+trieunnh5903@users.noreply.github.com>
2023-09-06 09:56:25 +0700

    .

--- src/screens/favourite/Favourite.js
+++ src/screens/favourite/Favourite.js
@@ -1,140 +1,138 @@
 import {
-  StyleSheet, Text, View,
+  StyleSheet,
+  Text,
+  View,
   Image,
   TouchableOpacity,
   FlatList,
-  SafeAreaView
-} from 'react-native'
-import React, { useCallback, useState } from 'react'
-import { COLORS, FONTS, SIZES, icons } from '../../config'
-import { ButtonIcon, Header } from '../../components'
-import { useDispatch, useSelector } from 'react-redux'
-import { removeFromFavorite } from '../../redux/slice/userSlice'
+  SafeAreaView,
+} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {COLORS, FONTS, SIZES, icons} from '../../config';
+import {ButtonIcon, Header} from '../../components';
+import {useDispatch, useSelector} from 'react-redux';
+import {removeFromFavorite} from '../../redux/slice/userSlice';
 
 const Favourite = () => {
   const dispatch = useDispatch();
   const favoriteList = useSelector(state => state.user.favorite);
-  const _enerateArray = useCallback(
-    (n) => {
-      let arr = new Array(n);
-      for (let i = 0; i < n; i++) {
-        arr[i] = {
-          id: Math.random(),
-          name: "Hamburger",
-          description: "Chicken patty hamburger",
-          categories: [1, 2],
-          price: 15.99,
-          calories: 78,
-          isFavourite: i % 2 == 0 ? true : false,
-          image: "https://cdn.tgdd.vn/Products/Images/2565/85959/bhx/-202306190956004561_300x300.jpg"
-        }
-      }
-      return arr;
-    }, [])
+  const _enerateArray = useCallback(n => {
+    let arr = new Array(n);
+    for (let i = 0; i < n; i++) {
+      arr[i] = {
+        id: Math.random(),
+        name: 'Hamburger',
+        description: 'Chicken patty hamburger',
+        categories: [1, 2],
+        price: 15.99,
+        calories: 78,
+        isFavourite: i % 2 == 0 ? true : false,
+        image:
+          'https://cdn.tgdd.vn/Products/Images/2565/85959/bhx/-202306190956004561_300x300.jpg',
+      };
+    }
+    return arr;
+  }, []);
   // const [menuList, setMenuList] = useState(_enerateArray(3));
-  const onRemovePress = (itemToRemove) => {
-    dispatch(removeFromFavorite(itemToRemove))
-  }
-  const renderItem = ({ item }) => (
-    <View
-      style={styles.itemContainer}
-    >
+  const onRemovePress = itemToRemove => {
+    dispatch(removeFromFavorite(itemToRemove));
+  };
+  const renderItem = ({item}) => (
+    <View style={styles.itemContainer}>
       {/* image */}
       <Image
-        style={{
-          marginHorizontal: SIZES.radius,
-          width: 70,
-          height: 70,
-        }}
+        style={styles.itemImage}
         source={{
-          uri:
-            item.image
-        }}></Image>
+          uri: item.image,
+        }}
+      />
       {/* content */}
-      <View style={{ flex: 1 }}>
+      <View style={{flex: 1}}>
         <Text
           style={{
             color: COLORS.blackText,
-            ...FONTS.label_large
-          }}
-        >Thùng 30 gói mì Hảo Hảo tôm chua cay 75g</Text>
-        <TouchableOpacity
-          style={{
-            alignSelf: 'flex-start',
-            flexDirection: 'row',
-            alignItems: 'center',
-          }}
-        >
+            ...FONTS.label_large,
+          }}>
+          Thùng 30 gói mì Hảo Hảo tôm chua cay 75g
+        </Text>
+        <TouchableOpacity style={styles.btnDetail}>
           <Text
             style={{
               color: COLORS.darkGray,
-              ...FONTS.label_medium
-            }}
-          >Xem chi tiết</Text>
-          <Image source={icons.down_arrow}
-            style={{ width: 18, height: 18, tintColor: COLORS.black }} />
+              ...FONTS.label_medium,
+            }}>
+            Xem chi tiết
+          </Text>
+          <Image
+            source={icons.down_arrow}
+            style={{width: 18, height: 18, tintColor: COLORS.black}}
+          />
         </TouchableOpacity>
       </View>
       {/* btn favourite */}
       <TouchableOpacity
         onPress={() => onRemovePress(item)}
-        style={{
-          padding: SIZES.base,
-          backgroundColor: COLORS.lightGray2,
-          marginRight: SIZES.radius,
-          borderRadius: 100,
-        }}>
-        <Image
-          source={icons.favourite_fill}
-          style={{
-            width: 24,
-            height: 24,
-            tintColor: COLORS.primary
-          }}
-        />
+        style={styles.btnFavorite}>
+        <Image source={icons.favourite_fill} style={styles.btnFavoriteRemove} />
       </TouchableOpacity>
     </View>
-  )
+  );
 
   return (
     <SafeAreaView style={styles.container}>
-      <Header
-        title={"Yêu thích"}
-      />
-      {
-        favoriteList.length ? (
-          <FlatList
-            data={favoriteList}
-            keyExtractor={(item, index) => `${index}`}
-            showsVerticalScrollIndicator={false}
-            renderItem={renderItem}
-          />
-        ) : (
-          <View
+      <Header title={'Yêu thích'} />
+      {favoriteList.length ? (
+        <FlatList
+          data={favoriteList}
+          keyExtractor={(item, index) => `${index}`}
+          showsVerticalScrollIndicator={false}
+          renderItem={renderItem}
+        />
+      ) : (
+        <View style={styles.emptyText}>
+          <Text
             style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            <Text
-              style={{
-                color: COLORS.black,
-                ...FONTS.headline_small,
-              }}
-            >Danh sách của bạn trống</Text>
-          </View>
-        )
-      }
-
-
+              color: COLORS.black,
+              ...FONTS.headline_small,
+            }}>
+            Danh sách của bạn trống
+          </Text>
+        </View>
+      )}
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Favourite
+export default Favourite;
 
 const styles = StyleSheet.create({
+  itemImage: {
+    marginHorizontal: SIZES.radius,
+    width: 70,
+    height: 70,
+  },
+
+  emptyText: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnDetail: {
+    alignSelf: 'flex-start',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  btnFavorite: {
+    padding: SIZES.base,
+    backgroundColor: COLORS.lightGray2,
+    marginRight: SIZES.radius,
+    borderRadius: 100,
+  },
+  btnFavoriteRemove: {
+    width: 24,
+    height: 24,
+    tintColor: COLORS.primary,
+  },
   itemContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -142,11 +140,11 @@ const styles = StyleSheet.create({
     paddingVertical: SIZES.radius,
     borderBottomWidth: 1,
     borderColor: COLORS.lightGray2,
-    backgroundColor: COLORS.white
+    backgroundColor: COLORS.white,
   },
 
   container: {
     flex: 1,
-    backgroundColor: COLORS.white
-  }
-})
+    backgroundColor: COLORS.white,
+  },
+});
--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,36 +1,23 @@
 import {
-  StyleSheet, Text, View,
+  StyleSheet,
+  Text,
+  View,
   SafeAreaView,
   Image,
   TouchableOpacity,
   ScrollView,
-  Switch
-} from 'react-native'
-import React, { useState } from 'react'
-import data from '../../data'
-import { COLORS, FONTS, SIZES, icons } from '../../config'
+} from 'react-native';
+import React, {useState} from 'react';
+import data from '../../data';
+import {COLORS, FONTS, SIZES, icons} from '../../config';
 
-const OptionItem = ({ icon, label }) => (
-  <TouchableOpacity
-    style={styles.optionItemWrapper}
-  >
-    <Image
-      source={icon}
-      style={styles.icon} />
-    <Text
-      style={{
-        marginLeft: SIZES.radius,
-        color: COLORS.blackText,
-        ...FONTS.label_large,
-        flex: 1
-      }}
-    >{label}
-    </Text>
-    <Image
-      source={icons.chevron_right}
-      style={styles.icon} />
+const OptionItem = ({icon, label}) => (
+  <TouchableOpacity style={styles.optionItemWrapper}>
+    <Image source={icon} style={styles.icon} />
+    <Text style={styles.itemLabel}>{label}</Text>
+    <Image source={icons.chevron_right} style={styles.icon} />
   </TouchableOpacity>
-)
+);
 
 const Profile = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -39,76 +26,75 @@ const Profile = () => {
       <ScrollView>
         <TouchableOpacity
           onPress={() => setIsDarkMode(!isDarkMode)}
-          style={{
-            paddingTop: SIZES.radius,
-            paddingHorizontal: SIZES.padding,
-            alignSelf: 'flex-end'
-          }}>
+          style={styles.btnDarkMode}>
           <Image
             source={isDarkMode ? icons.light_mode : icons.dark_mode}
-            style={styles.icon} />
+            style={styles.icon}
+          />
         </TouchableOpacity>
 
         {/* profile */}
-        <View
-          style={styles.profileWrapper}
-        >
+        <View style={styles.profileWrapper}>
           <Image
             style={styles.profile}
-            source={{ uri: data.myProfile.profile_image }}>
-          </Image>
+            source={{uri: data.myProfile.profile_image}}
+          />
           <View>
-            <Text
-              style={{
-                color: COLORS.blackText,
-                ...FONTS.title_large,
-                fontWeight: 'bold'
-              }}
-            >{data.myProfile.name}</Text>
+            <Text style={styles.profileName}>{data.myProfile.name}</Text>
             <Text
               style={{
                 color: COLORS.blackText,
                 ...FONTS.label_large,
-             
-              }}
-            >Thành viên</Text>
+              }}>
+              Thành viên
+            </Text>
           </View>
         </View>
         {/* user options */}
         {/* system options */}
-        <View
-          style={styles.optionWrapper}>
-          <OptionItem icon={icons.person} label={"Chỉnh sửa thông tin"} />
-          <View style={{ height: SIZES.radius }} />
-          <OptionItem icon={icons.wallet} label={"Phương thức thanh toán"} />
-          <View style={{ height: SIZES.radius }} />
-          <OptionItem icon={icons.address} label={"Địa chỉ"} />
+        <View style={styles.optionWrapper}>
+          <OptionItem icon={icons.person} label={'Chỉnh sửa thông tin'} />
+          <View style={{height: SIZES.radius}} />
+          <OptionItem icon={icons.wallet} label={'Phương thức thanh toán'} />
+          <View style={{height: SIZES.radius}} />
+          <OptionItem icon={icons.address} label={'Địa chỉ'} />
         </View>
         <View style={styles.optionWrapper}>
-          <OptionItem icon={icons.policy} label={"Điều khoản và Chính sách"} />
-          <View style={{ height: SIZES.radius }} />
-          <OptionItem icon={icons.support} label={"Hỗ trợ"} />
-          <View style={{ height: SIZES.radius }} />
-          <OptionItem icon={icons.notification_w400} label={"Thông báo"} />
+          <OptionItem icon={icons.policy} label={'Điều khoản và Chính sách'} />
+          <View style={{height: SIZES.radius}} />
+          <OptionItem icon={icons.support} label={'Hỗ trợ'} />
+          <View style={{height: SIZES.radius}} />
+          <OptionItem icon={icons.notification_w400} label={'Thông báo'} />
         </View>
         <View style={styles.optionWrapper}>
-          <OptionItem icon={icons.logout} label={"Đăng xuất"} />
+          <OptionItem icon={icons.logout} label={'Đăng xuất'} />
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
 
 const styles = StyleSheet.create({
+  itemLabel: {
+    marginLeft: SIZES.radius,
+    color: COLORS.blackText,
+    ...FONTS.label_large,
+    flex: 1,
+  },
 
+  btnDarkMode: {
+    paddingTop: SIZES.radius,
+    paddingHorizontal: SIZES.padding,
+    alignSelf: 'flex-end',
+  },
   optionWrapper: {
     padding: 20,
     backgroundColor: COLORS.lightGray2,
     marginHorizontal: SIZES.padding,
     marginTop: SIZES.padding,
-    borderRadius: SIZES.radius
+    borderRadius: SIZES.radius,
   },
 
   optionItemWrapper: {
@@ -119,9 +105,14 @@ const styles = StyleSheet.create({
   icon: {
     width: 32,
     height: 32,
-    tintColor: COLORS.black
+    tintColor: COLORS.black,
   },
 
+  profileName: {
+    color: COLORS.blackText,
+    ...FONTS.title_large,
+    fontWeight: 'bold',
+  },
   profileWrapper: {
     flexDirection: 'row',
     // justifyContent: 'center',
@@ -133,10 +124,10 @@ const styles = StyleSheet.create({
   profile: {
     width: 80,
     height: 80,
-    borderRadius: 80
+    borderRadius: 80,
   },
   container: {
     flex: 1,
-    backgroundColor: COLORS.white
-  }
-})
+    backgroundColor: COLORS.white,
+  },
+});
